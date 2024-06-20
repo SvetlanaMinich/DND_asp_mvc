f1cc93d6bd463031b3b610b05e4493c0f0d876ae
Svetlana Minich <imsvetam@mail.ru>
2024-06-20 13:34:54 +0300

    start app

---
+++ dnd/dnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
